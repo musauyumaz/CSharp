00b157b17646ac5e66c346c2c6dd9944ad264a6e
Musa UYUMAZ <78364995+musauyumaz@users.noreply.github.com>
2022-06-25 21:23:27 +0300

    25/06/2022

---
+++ Murat Yücedağ/MvcProjeKampi/BusinessLayer/Concrete/CategoryManager.cs
@@ -0,0 +1,34 @@
+﻿using DataAccessLayer.Concrete;
+using DataAccessLayer.Repositories;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class CategoryManager
+    {
+        GenericRepository<Category> repo = new DataAccessLayer.Repositories.GenericRepository<Category>();
+
+        public List<Category> GetAllBl()
+        {
+            return repo.List();
+        }
+        public void CategoryAddBl(Category category)
+        {
+            //if (category.CategoryName =="" || category.CategoryName.Length<=3||
+            //    category.CategoryDescription=="" || category.CategoryName.Length >=51)
+            //{
+            //    //Hata mesajı
+            //}
+            //else
+            //{
+            //    repo.Insert(category);
+            //}
+            repo.Insert(category);
+        }
+    }
+}
---
+++ Murat Yücedağ/MvcProjeKampi/MvcProjeKampi/Controllers/CategoryController.cs
@@ -0,0 +1,36 @@
+﻿using BusinessLayer.Concrete;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcProjeKampi.Controllers
+{
+    public class CategoryController : Controller
+    {
+        // GET: Category
+        CategoryManager categoryManager = new CategoryManager();
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult GetCategoryList()
+        {
+            var categoryValues = categoryManager.GetAllBl();
+            return View(categoryValues);
+        }
+        [HttpGet]
+        public ActionResult AddCategory()//Sayfa açılırken devreye girecek
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult AddCategory(Category category)//Sayfada butona tıklanırsa devreye girecek
+        {
+            categoryManager.CategoryAddBl(category);
+            return RedirectToAction("GetCategoryList");
+        }
+    }
+}
